0619cf81ec3638386d3606f2c39eff57a43be5f0
codeblooded1729 <39694928+codeblooded1729@users.noreply.github.com>
2022-02-03 19:01:59 +0530

    Create script.py

---
+++ codewars/script.py
@@ -0,0 +1,12 @@
+#https://www.codewars.com/kata/54ce4c6804fcc440a1000ecb/python
+
+def encode(s):
+    lst = sorted( s[i or len(s):] + s[:i or len(s)] for i in reversed(range(len(s))) )
+    return ''.join(ss[-1] for ss in lst), s and lst.index(s) or 0
+
+def decode(s, n):
+    out, lst = [], sorted((c,i) for i,c in enumerate(s))
+    for _ in range(len(s)):
+        c,n = lst[n]
+        out.append(c)
+    return ''.join(out)
